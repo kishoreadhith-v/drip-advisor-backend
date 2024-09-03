6bb9bb5195d5d6e8c6c600dcd99bdf6cead3e320
kishoreadhith-v <kishoreadhithvijayakumaar@gmail.com>
2024-09-03 22:08:26 +0530

    env, db connection setup and test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify
+import traceback
+from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+
+client = MongoClient(os.getenv('MONGO_URI'))
+db = client['dev']
+
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Hello, New World!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/env', methods=['GET'])
+def env():
+    return os.getenv('TEST_VAR')
+
+@app.route('/test_get', methods=['GET'])
+def test_get():
+    users = db.users.find()
+    userlist = list(users)
+    for user in userlist:
+        del user['_id']
+    
+    return jsonify(userlist)
+
+
+def error_stack(error):
+    stack_trace = traceback.format_exc()
+    response = {
+        'error': error,
+        'stack_trace': stack_trace
+    }
+    return jsonify(response), 500
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return error_stack(str(e))
